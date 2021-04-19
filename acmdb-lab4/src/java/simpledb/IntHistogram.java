60a737b57d0957ac21a45492cebac39cc3226c07
Runzhe <runzhe2000@gmail.com>
2021-04-19 16:54:01 +0800

    [DONE] lab4 ex1

--- acmdb-lab4/src/java/simpledb/IntHistogram.java
+++ acmdb-lab4/src/java/simpledb/IntHistogram.java
@@ -4,6 +4,15 @@ package simpledb;
  */
 public class IntHistogram {
 
+	private int min;
+	private int max;
+	private int buckets;
+	private int interval;
+	private int totalNum;
+	private int[] left;
+	private int[] right;
+	private int [] count;
+
     /**
      * Create a new IntHistogram.
      * 
@@ -22,7 +31,27 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+		this.min = min;
+		this.max = max;
+		this.buckets = Math.min(buckets, max - min + 1);
+		left = new int[this.buckets];
+		right = new int[this.buckets];
+		count = new int[this.buckets];
+		this.interval = (max - min + 1) / this.buckets;
+
+		for(int i = 0; i < this.buckets; i++){
+			left[i] = min + interval * i;
+			right[i] = left[i] + interval - 1;
+			count[i] = 0;
+		}
+		right[this.buckets - 1] = max;
     }
+
+    private int getIdx(int v){
+    	assert min <= v && v <= max;
+    	int idx = (v - min) / interval;
+    	return Math.min(idx, this.buckets - 1);
+	}
 
     /**
      * Add a value to the set of values that you are keeping a histogram of.
@@ -30,6 +59,9 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+		int idx = getIdx(v);
+		count[idx]++;
+		totalNum++;
     }
 
     /**
@@ -43,9 +75,44 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
     	// some code goes here
-        return -1.0;
+		switch (op){
+			case EQUALS:
+				if(v < min || v > max) return 0.0;
+				else {
+					int idx = getIdx(v);
+					return 1.0 * count[idx] / (right[idx] - left[idx] + 1) / totalNum;
+				}
+			case NOT_EQUALS:
+				if(v < min || v > max) return 1.0;
+				else{
+					int idx = getIdx(v);
+					return 1.0 - 1.0 * count[idx] / (right[idx] - left[idx] + 1) / totalNum;
+				}
+			case LESS_THAN:
+			case GREATER_THAN_OR_EQ:
+				if(v < min) return op == Predicate.Op.LESS_THAN ? 0.0 : 1.0;
+				else if(v > max) return op == Predicate.Op.LESS_THAN ? 1.0 : 0.0;
+				else{
+					int idx = getIdx(v);
+					double sum = 0;
+					for(int i = 0; i < idx; i++) sum += count[i];
+					sum += 1.0 * count[idx] / (right[idx] - left[idx] + 1) * (v - left[idx]);
+					return op == Predicate.Op.LESS_THAN ? sum / totalNum : 1.0 - sum / totalNum;
+				}
+			case LESS_THAN_OR_EQ:
+			case GREATER_THAN:
+				if(v < min) return op == Predicate.Op.LESS_THAN_OR_EQ ? 0.0 : 1.0;
+				else if(v > max) return op == Predicate.Op.LESS_THAN_OR_EQ ? 1.0 : 0.0;
+				else{
+					int idx = getIdx(v);
+					double sum = 0;
+					for(int i = 0; i < idx; i++) sum += count[i];
+					sum += 1.0 * count[idx] / (right[idx] - left[idx] + 1) * (v - left[idx] + 1);
+					return op == Predicate.Op.LESS_THAN_OR_EQ ? sum / totalNum : 1.0 - sum / totalNum;
+				}
+		}
+		return 2333333;
     }
     
     /**
